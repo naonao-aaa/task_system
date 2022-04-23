869b4214964ec43b824aefa7f21f8c64a8b93698
naonao-aaa <naonao1814naonao@gmail.com>
2022-04-23 11:02:33 +0900

    微調整

--- app/Task.php
+++ app/Task.php
@@ -8,7 +8,7 @@ class Task extends Model
 {
     //Modelで扱うカラム名（カラムに対応する変数名）を設定します。このプロパティに無い変数名は無視されるようになります。
     protected $fillable = [
-        'id', 'name', 'description', 'user_id', 'category_id', 'status_id', 'progress', 'man-hours', 'deadline'
+        'id', 'name', 'description', 'user_id', 'category_id', 'status_id', 'progress', 'man_hours', 'deadline'
     ];
 
     //casts（型を変換することをキャストという。DBの日時・日付型はphpでは文字列として扱われるが、この設定をすることで自動でそれぞれの型に変換してくれる。）
